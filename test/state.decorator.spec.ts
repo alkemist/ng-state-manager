8a0969aaa7e58afb6e0fadfe3774d67f73f02a33
Jérémy ACHAIN <achain.jeremy@gmail.com>
2023-09-18 18:53:32 +0200

    Improves

--- test/state.decorator.spec.ts
+++ test/state.decorator.spec.ts
@@ -1,47 +1,50 @@
-import { aStringValueDefault, ExampleComponent } from './test-data';
-import { effect } from '@angular/core';
-import { setUpSignalTesting, SignalTesting } from './setup-effect';
+import {aStringValueDefault, ExampleComponent} from './test-data.js';
+import {effect, Injector} from '@angular/core';
+import {setUpSignalTesting, SignalTesting} from './setup-effect.js';
 
 
 describe("State Decorator", () => {
-  let signalTesting: SignalTesting;
+    let signalTesting: SignalTesting;
+    let onChangeSpy: jest.SpyInstance;
 
-  beforeEach(() => {
-    signalTesting = setUpSignalTesting();
-  })
-
-  it('should work', () => {
-
-    signalTesting.runInTestingInjectionContext((injector) => {
-      const exampleComponent = new ExampleComponent();
-      const aStringValueTest = 'test';
-      let aStringValueEffect = '';
-
-      effect(
-        () => {
-          aStringValueEffect = exampleComponent.aStringValueObserver();
-        },
-        { injector }
-      );
-      signalTesting.flushEffects();
-
-      expect(exampleComponent.aStringValueObserver()).toEqual(aStringValueDefault)
-      expect(exampleComponent.aStringValueComputed()).toEqual(aStringValueDefault)
-      expect(aStringValueEffect).toEqual(aStringValueDefault)
-
-      exampleComponent.dispatch(aStringValueTest);
-      signalTesting.flushEffects();
-
-      expect(exampleComponent.aStringValueObserver()).toEqual(aStringValueTest)
-      expect(exampleComponent.aStringValueComputed()).toEqual(aStringValueTest)
-      expect(aStringValueEffect).toEqual(aStringValueTest)
-
-      exampleComponent.dispatch(aStringValueTest);
-      signalTesting.flushEffects();
-
-      expect(exampleComponent.aStringValueObserver()).toEqual(aStringValueDefault)
-      expect(exampleComponent.aStringValueComputed()).toEqual(aStringValueDefault)
-      expect(aStringValueEffect).toEqual(aStringValueDefault)
+    beforeEach(() => {
+        signalTesting = setUpSignalTesting();
     })
-  })
+
+    it('should work', () => {
+        signalTesting.runInTestingInjectionContext((injector: Injector) => {
+            const exampleComponent = new ExampleComponent();
+            const aStringValueTest = 'test';
+            let aStringValueEffect = '';
+
+            onChangeSpy = jest.spyOn(exampleComponent, 'onChange');
+
+            effect(
+                () => {
+                    aStringValueEffect = exampleComponent.aStringValueObserver();
+                },
+                {injector}
+            );
+            signalTesting.flushEffects();
+
+            expect(exampleComponent.aStringValueObserver()).toEqual(aStringValueDefault)
+            expect(exampleComponent.aStringValueComputed()).toEqual(aStringValueDefault)
+            expect(aStringValueEffect).toEqual(aStringValueDefault);
+            expect(onChangeSpy).toBeCalledTimes(1);
+
+            onChangeSpy.mockReset();
+            exampleComponent.dispatch(aStringValueTest);
+            signalTesting.flushEffects();
+
+            expect(exampleComponent.aStringValueObserver()).toEqual(aStringValueTest)
+            expect(exampleComponent.aStringValueComputed()).toEqual(aStringValueTest)
+            expect(aStringValueEffect).toEqual(aStringValueTest);
+            expect(onChangeSpy).toBeCalledTimes(1);
+
+            onChangeSpy.mockReset();
+            exampleComponent.dispatch(aStringValueTest);
+
+            expect(onChangeSpy).not.toBeCalled();
+        })
+    })
 });
--- test/test-data.ts
+++ test/test-data.ts
@@ -1,57 +1,62 @@
-import { State } from "../src/state.decorator.js";
-import { BaseState } from "../src/state.base.js";
-import { Select } from "../src/state-select.decorator.js";
-import { Action } from "../src/state-action.decorator.js";
-import { Observe } from "../src/state-observe.decorator.js";
-import { StateContext } from "../src/state.context.js";
-import { StateManager } from "../src/state-manager.js";
-import { ValueRecord } from "@alkemist/compare-engine";
-import { StateDispatch } from '../src/state-dispatch';
-import { computed, Signal, WritableSignal } from '@angular/core';
+import {State} from "../src/state.decorator.js";
+import {BaseState} from "../src/state.base.js";
+import {Select} from "../src/state-select.decorator.js";
+import {Action} from "../src/state-action.decorator.js";
+import {Observe} from "../src/state-observe.decorator.js";
+import {StateContext} from "../src/state.context.js";
+import {StateManager} from "../src/state-manager.js";
+import {ValueRecord} from "@alkemist/compare-engine";
+import {StateDispatch} from '../src/state-dispatch.js';
+import {computed, Signal, WritableSignal} from '@angular/core';
 
 interface ExampleStateInterface extends ValueRecord {
-  aStringValue: string
+    aStringValue: string
 }
 
 export const exampleStateName = 'example';
 export const aStringValueDefault = 'init';
 
 @State<ExampleStateInterface>({
-  name: exampleStateName,
-  defaults: {
-    aStringValue: aStringValueDefault
-  }
+    name: exampleStateName,
+    defaults: {
+        aStringValue: aStringValueDefault
+    }
 })
 export class ExampleState extends BaseState {
-  @Select('aStringValue')
-  static aStringValueSelector(state: ExampleStateInterface): string {
-    return state.aStringValue;
-  }
+    @Select('aStringValue')
+    static aStringValueSelector(state: ExampleStateInterface): string {
+        return state.aStringValue;
+    }
 
-  @Action('An action')
-  static aAction(context: StateContext<ExampleStateInterface>, payload: string): StateContext<ExampleStateInterface> {
-    context.setState({
-      aStringValue: payload
-    })
-    return context;
-  }
+    @Action('An action')
+    static aAction(context: StateContext<ExampleStateInterface>, payload: string): StateContext<ExampleStateInterface> {
+        context.setState({
+            aStringValue: payload
+        })
+        return context;
+    }
 }
 
 export class ExampleComponent {
-  @Observe(ExampleState, ExampleState.aStringValueSelector)
-  aStringValueObserver!: WritableSignal<string>;
+    @Observe(ExampleState, ExampleState.aStringValueSelector)
+    aStringValueObserver!: WritableSignal<string>;
 
-  aStringValueComputed: Signal<string>;
+    aStringValueComputed: Signal<string>;
 
-  constructor() {
-    this.aStringValueComputed = computed(() => {
-      return this.aStringValueObserver();
-    });
-  }
+    constructor() {
+        this.aStringValueComputed = computed(() => {
+            this.onChange(this.aStringValueObserver());
+            return this.aStringValueObserver();
+        });
+    }
 
-  dispatch(value: string) {
-    StateManager.dispatch(
-      new StateDispatch(ExampleState, ExampleState.aAction, value)
-    )
-  }
+    onChange(value: string) {
+        return value;
+    }
+
+    dispatch(value: string) {
+        StateManager.dispatch(
+            new StateDispatch(ExampleState, ExampleState.aAction, value)
+        )
+    }
 }
